fbe9e58095fba83a0e3d5faae621f542bbbd46c6
JEETDESAI25 <jeetdesai25@gmail.com>
2023-05-09 20:25:26 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import InputFeild from "./components/inputFeild";
 
 const App: React.FC  = () => (
     <div className="App">
         <span className="heading">Taskify</span>
+        <InputFeild/>
     </div>
 );
 
